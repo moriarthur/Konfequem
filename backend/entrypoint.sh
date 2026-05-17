0d10d4c090dc3651c4002d0e1f034ecace545168
unlxud <>
2026-05-17 22:37:03 +0200

    fix: default to port 8000 in entrypoint for local Docker
    Render sets $PORT but local docker-compose doesn't. Also add
    staticfiles/ to gitignore.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_superuser
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
